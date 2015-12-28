8185a53995a7447d93435279e4fccb79d6ff85eb
Assaf <assafdd@gmail.com>
2015-12-29 00:46:05 +0200

    Create SalesforceForecastGoesDown.js

---
+++ SalesforceForecastGoesDown.js
@@ -0,0 +1,5 @@
+var oldForecast = oldDataStore, message // oldDataStore is the value save in previous run
+var newForecast = parseInt(element.textContent.replace(/\$|\,/g,""));
+if (newForecast < oldForecast)
+    message = `Forecast decline by ${oldForecast-newForecast}$ to ${newForecast}$`
+({message: message, newDataStore: newForecast}); // return value 
