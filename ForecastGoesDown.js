b749a85ac3204fef738f6c6dca4b34221329f68b
Assaf <assafdd@gmail.com>
2015-12-29 00:48:32 +0200

    Salesforce Forecast Goes Down exmaple
    Salesforce Forecast Goes Down exmaple

--- ForecastGoesDown.js
+++ ForecastGoesDown.js
@@ -1,4 +1,4 @@
-var oldForecast = oldDataStore, message // oldDataStore is the value save in previous run
+var oldForecast = oldDataStore, message // oldDataStore is the value save in previous run 
 var newForecast = parseInt(element.textContent.replace(/\$|\,/g,""));  
 if (newForecast < oldForecast)
     message = `Forecast decline by ${oldForecast-newForecast}$ to ${newForecast}$`
